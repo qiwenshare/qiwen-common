52db8577683255c8c70851885c1cfc7f59a757c0
MAC <1162714483@qq.com>
2022-01-10 20:53:50 +0800

    style(代码优化): 统一结果返回

--- src/main/java/com/qiwenshare/common/result/RestResult.java
+++ src/main/java/com/qiwenshare/common/result/RestResult.java
@@ -1,6 +1,5 @@
 package com.qiwenshare.common.result;
 
-//import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
--- src/main/java/com/qiwenshare/common/result/ResultCodeEnum.java
+++ src/main/java/com/qiwenshare/common/result/ResultCodeEnum.java
@@ -13,12 +13,14 @@ public enum ResultCodeEnum {
     DAO_SELECT_ERROR(false, 100001, "查询数据异常"),
     DAO_UPDATE_ERROR(false, 100002, "更新数据异常"),
     DAO_DELETE_ERROR(false, 100003, "删除数据异常"),
+    NULL_POINT(false, 100004, "空指针异常"),
+    INDEX_OUT_OF_BOUNDS(false, 100005, "下标越界异常"),
+    REQUEST_TIMEOUT(false, 100006, "请求超时"),
+    PARAM_ERROR(false,100007,"参数错误"),
+    NOT_INIT_DATA(false, 100008, "数据未初始化"),
 
-    PARAM_ERROR(false,200002,"参数错误"),
-    NULL_POINT(false, 200003, "空指针异常"),
-    INDEX_OUT_OF_BOUNDS(false, 200004, "下标越界异常"),
-    REQUEST_TIMEOUT(false, 200005, "请求超时"),
-    NOT_LOGIN_ERROR(false, 200006, "未登录异常"),
+    USER_FORBIDDEN(false,200001,"用户被禁用"),
+    NOT_LOGIN_ERROR(false, 200002, "未登录"),
     ;
 
     // 响应是否成功
