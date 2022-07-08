c0d49d444845f0bd04eddef22235e5d7e0083c70
18829291817 <1162714483@qq.com>
2022-07-08 13:00:04 +0000

    增加Music工具类

--- src/main/java/com/qiwenshare/common/operation/CrawHtmlOperation.java
+++ src/main/java/com/qiwenshare/common/operation/CrawHtmlOperation.java
@@ -35,4 +35,8 @@ public class CrawHtmlOperation {
         pageXml = StringUtil.replaceBlank(pageXml);
         return pageXml;
     }
+
+    public static void main(String[] args) {
+        System.out.println(crawHtmlByUrl("https://www.baidu.com/"));
+    }
 }
